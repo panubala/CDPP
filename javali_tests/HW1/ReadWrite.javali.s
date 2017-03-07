9b3c496f200e995a3532e41126c3c6b385cfc65a
panubala <panuyabalasuntharam@public-docking-cx-0714.ethz.ch>
2017-03-07 13:35:53 +0100

    Read function

--- javali_tests/HW1/ReadWrite.javali.s
+++ javali_tests/HW1/ReadWrite.javali.s
@@ -4,71 +4,32 @@
     .string "%d"
 .LC1:
     .string "\n"
+.LC2:
+    .string "%d"
     .globl _main
 
 _main:
     pushl %ebp
     movl %esp, %ebp
       # Emitting (...)
-        # Emitting i0 = 5
-          # Emitting i0
-          addl $-4, %esp
-          # Emitting 5
-          movl $5, %esi
-        movl %esi, -4(%ebp)
         # Emitting i1 = read()
           # Emitting i1
           addl $-4, %esp
           # Emitting read()
-          pushl %esp
+          subl $4, %esp
+          leal (%esp), %esi
+          pushl %esi
+          pushl $.LC2
           call _scanf
-          movl 0(%esp), %esi
-        movl %esi, -8(%ebp)
-<<<<<<< HEAD
+          addl $4, %esp
+          movl -8(%ebp), %edx
+        movl %edx, -4(%ebp)
         # Emitting write(i1)
           # Emitting i1
-          movl -8(%ebp), %edi
+          movl -4(%ebp), %edi
         pushl %edi
-        call _printf
-=======
-        # Emitting r1 = (i0 + i1)
-          # Emitting r1
-          movl -36(%ebp), %edi
-          # Emitting (i0 + i1)
-            # Emitting i0
-            movl -4(%ebp), %esi
-            # Emitting i1
-            movl -8(%ebp), %edx
-          pushl %esi
-          pushl %edx
-          movl 4(%esp), %esi
-          addl 0(%esp), %esi
-        movl %esi, -36(%ebp)
-        # Emitting write(r1)
-          # Emitting r1
-          movl -36(%ebp), %edi
-        pushl %edi
-        call _printf
-        # Emitting writeln()
-        pushl $.LC1
-        call _printf
-        # Emitting write((r1 - 3))
-          # Emitting (r1 - 3)
-            # Emitting r1
-            movl -36(%ebp), %edi
-            # Emitting 3
-            movl $3, %esi
-          pushl %edi
-          pushl %esi
-          movl 4(%esp), %edi
-          subl 0(%esp), %edi
-        pushl %edi
-        call _printf
-        # Emitting writeln()
-        pushl $.LC1
         call _printf
     movl %ebp, %esp
->>>>>>> 863530a4320df1f0a3dcaa072a5c43369f5a1b1f
     movl $0, %eax
     popl %ebp
     ret
