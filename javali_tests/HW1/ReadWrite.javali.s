32e49c89acd0d03a337f876690a37ae11eada44d
p.gruntz <patrick.gruntz@web.de>
2017-03-07 13:36:51 +0100

    testing

--- javali_tests/HW1/ReadWrite.javali.s
+++ javali_tests/HW1/ReadWrite.javali.s
@@ -24,51 +24,12 @@ _main:
           call _scanf
           movl 0(%esp), %esi
         movl %esi, -8(%ebp)
-<<<<<<< HEAD
         # Emitting write(i1)
           # Emitting i1
           movl -8(%ebp), %edi
         pushl %edi
         call _printf
-=======
-        # Emitting r1 = (i0 + i1)
-          # Emitting r1
-          movl -36(%ebp), %edi
-          # Emitting (i0 + i1)
-            # Emitting i0
-            movl -4(%ebp), %esi
-            # Emitting i1
-            movl -8(%ebp), %edx
-          pushl %esi
-          pushl %edx
-          movl 4(%esp), %esi
-          addl 0(%esp), %esi
-        movl %esi, -36(%ebp)
-        # Emitting write(r1)
-          # Emitting r1
-          movl -36(%ebp), %edi
-        pushl %edi
-        call _printf
-        # Emitting writeln()
-        pushl $.LC1
-        call _printf
-        # Emitting write((r1 - 3))
-          # Emitting (r1 - 3)
-            # Emitting r1
-            movl -36(%ebp), %edi
-            # Emitting 3
-            movl $3, %esi
-          pushl %edi
-          pushl %esi
-          movl 4(%esp), %edi
-          subl 0(%esp), %edi
-        pushl %edi
-        call _printf
-        # Emitting writeln()
-        pushl $.LC1
-        call _printf
-    movl %ebp, %esp
->>>>>>> 863530a4320df1f0a3dcaa072a5c43369f5a1b1f
+
     movl $0, %eax
     popl %ebp
     ret
