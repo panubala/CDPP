0fcd178272c20028594c338aa9a3d1c1ea12c082
p.gruntz <patrick.gruntz@web.de>
2017-05-27 15:00:24 +0200

    Task 1 - Return Stmt

--- HW5/src/cd/transform/CfgBuilder.java
+++ HW5/src/cd/transform/CfgBuilder.java
@@ -20,12 +20,16 @@ public class CfgBuilder extends AstVisitor<Void, ControlFlowGraph> {
 		System.out.println("==Build");
 		ControlFlowGraph cfg = mdecl.cfg = new ControlFlowGraph();
 
-		cfg.start = cfg.end = cfg.newBlock();
-		BasicBlock finalBlock = cfg.newBlock();
-		
-		visit(mdecl.body(), cfg);	
-		
-		cfg.connect(cfg.end, finalBlock);
+		cfg.start = cfg.newBlock();
+		cfg.end = cfg.newBlock();
+
+		BasicBlock startBlock = cfg.start;
+		BasicBlock finalBlock = cfg.end;
+
+		visit(mdecl.body(), cfg);
+		if (!cfg.start.successors.contains(cfg.end))
+			cfg.connect(cfg.start, finalBlock);
+		cfg.start = startBlock;
 
 		// CFG and AST are not synchronized, only use CFG from now on
 		mdecl.setBody(null);
@@ -34,78 +38,94 @@ public class CfgBuilder extends AstVisitor<Void, ControlFlowGraph> {
 
 	@Override
 	public Void assign(Assign ast, ControlFlowGraph arg) {
-		arg.end.stmts.add(ast);
+		if (!arg.start.successors.contains(arg.end)) {
+			arg.start.stmts.add(ast);
+		}
 		return null;
 	}
 
 	@Override
 	public Void builtInWrite(BuiltInWrite ast, ControlFlowGraph arg) {
-		arg.end.stmts.add(ast);
+		if (!arg.start.successors.contains(arg.end)) {
+			arg.start.stmts.add(ast);
+		}
 		return null;
 	}
 
 	@Override
 	public Void builtInWriteln(BuiltInWriteln ast, ControlFlowGraph arg) {
-		arg.end.stmts.add(ast);
+		if (!arg.start.successors.contains(arg.end)) {
+			arg.start.stmts.add(ast);
+		}
 		return null;
 	}
 
 	@Override
 	public Void returnStmt(ReturnStmt ast, ControlFlowGraph arg) {
-		arg.end.stmts.add(ast);
+		if (!arg.start.successors.contains(arg.end)) {
+			arg.start.stmts.add(ast);
+			arg.connect(arg.start, arg.end);
+		}
 		return null;
 	}
-
 
 	@Override
 	public Void methodCall(MethodCall ast, ControlFlowGraph arg) {
-		arg.end.stmts.add(ast);
+		if (!arg.start.successors.contains(arg.end)) {
+			arg.start.stmts.add(ast);
+		}
 		return null;
 	}
-	
+
 	@Override
 	public Void nop(Nop ast, ControlFlowGraph arg) {
-		arg.end.stmts.add(ast);
+		if (!arg.start.successors.contains(arg.end)) {
+			arg.start.stmts.add(ast);
+		}
 		return null;
 	}
 
 	@Override
 	public Void ifElse(IfElse ast, ControlFlowGraph cfg) {
-		
-		BasicBlock condBlock = cfg.end;
-		cfg.terminateInCondition(condBlock, ast.condition());
-		
-		cfg.end = condBlock.trueSuccessor();
-		visit(ast.then(), cfg);
-		BasicBlock endTrue = cfg.end;
-		
-		cfg.end = condBlock.falseSuccessor();
-		visit(ast.otherwise(), cfg);
-		BasicBlock endFalse = cfg.end;
-		
-		BasicBlock after = cfg.newBlock();
-		cfg.connect(endTrue, after);
-		cfg.connect(endFalse, after);
-		
-		cfg.end = after;
-		
+		if (!cfg.start.successors.contains(cfg.end)) {
+			BasicBlock condBlock = cfg.start;
+			cfg.terminateInCondition(condBlock, ast.condition());
+
+			cfg.start = condBlock.trueSuccessor();
+			visit(ast.then(), cfg);
+			BasicBlock endTrue = cfg.start;
+
+			cfg.start = condBlock.falseSuccessor();
+			visit(ast.otherwise(), cfg);
+			BasicBlock endFalse = cfg.start;
+
+			BasicBlock after = cfg.newBlock();
+			if (!endTrue.successors.contains(cfg.end)) // not already terminated
+				cfg.connect(endTrue, after);
+			if (!endFalse.successors.contains(cfg.end))
+				cfg.connect(endFalse, after);
+
+			cfg.start = after;
+		}
 		return null;
 	}
 
 	@Override
 	public Void whileLoop(WhileLoop ast, ControlFlowGraph cfg) {
-		
-		BasicBlock loopBlock = cfg.newBlock();
-		cfg.connect(cfg.end, loopBlock);
-		
-		cfg.terminateInCondition(loopBlock, ast.condition());
-		
-		cfg.end = loopBlock.trueSuccessor();
-		visit(ast.body(), cfg);
-		cfg.connect(cfg.end, loopBlock);
-		
-		cfg.end = loopBlock.falseSuccessor();
-		
+		if (!cfg.start.successors.contains(cfg.end)) {
+			BasicBlock loopBlock = cfg.newBlock();
+			cfg.connect(cfg.start, loopBlock);
+
+			cfg.terminateInCondition(loopBlock, ast.condition());
+
+			cfg.start = loopBlock.trueSuccessor();
+			visit(ast.body(), cfg);
+			if (!cfg.start.successors.contains(cfg.end)) // Loop body didnt
+															// terminated
+				cfg.connect(cfg.start, loopBlock);
+
+			cfg.start = loopBlock.falseSuccessor();
+		}
 		return null;
 	}
 }
